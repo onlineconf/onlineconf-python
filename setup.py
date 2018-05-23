2bb345d9282deec923f30855e62a5e57fa4bced9
Andrey Oleynik <andrey.oleynik@corp.mail.ru>
2018-05-23 11:35:19 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='0.0.1',
     url='https://gitlab.corp.mail.ru/myspb/common/onlineconf',
     author='mail.ru',
+    packages=['.'],
     install_requires=[
-        'pure-cdb==2.0.1'
+        'pure-cdb==2.0.1',
+        'aiofiles==0.3.2'
     ]
 )
