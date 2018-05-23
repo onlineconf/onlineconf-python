83da919446e77b2bb20db16974a07934ddfaaa76
Andrey Oleynik <andrey.oleynik@corp.mail.ru>
2018-05-23 11:19:12 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='onlineconf',
+    version='0.0.1',
+    url='https://gitlab.corp.mail.ru/myspb/common/onlineconf',
+    author='mail.ru',
+    install_requires=[
+        'pure-cdb==2.0.1'
+    ]
+)
