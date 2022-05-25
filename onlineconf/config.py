284f42dd4546a2a5fcc108de4c82cb84ed216167
Anton Malko <anton.malko@corp.mail.ru>
2022-05-25 14:16:13 +0300

    update get_event_loop

--- onlineconf/config.py
+++ onlineconf/config.py
@@ -1,8 +1,7 @@
 import asyncio
-from typing import TYPE_CHECKING, Optional
 import json
 from contextlib import suppress
-from typing import Union, List, Tuple, Iterator, Optional
+from typing import TYPE_CHECKING, Any, Dict, Iterator, List, Optional, Tuple, Union
 
 import aiofiles
 import cdblib
@@ -18,16 +17,15 @@ __all__ = ("Config",)
 
 
 class Config:
-
     def __init__(
         self,
         filename: str,
         reload_interval: Optional[int] = None,
-        loop: Optional["AbstractEventLoop"]=None
-        ) -> None:
+        loop: Optional["AbstractEventLoop"] = None,
+    ) -> None:
         self._filename = filename
         self._reload_interval = reload_interval
-        self._reload_task = None
+        self._reload_task: Optional[asyncio.Task[None]] = None
         self._loop = loop if loop else get_event_loop()
 
     @classmethod
@@ -35,8 +33,8 @@ class Config:
         cls,
         filename: str,
         reload_interval: Optional[int] = 30,
-        loop: Optional["AbstractEventLoop"]=None
-        ) -> "Config":
+        loop: Optional["AbstractEventLoop"] = None,
+    ) -> "Config":
         """Read a cdb file and schedule periodic reload if needed"""
         _config = cls(filename=filename, reload_interval=reload_interval, loop=loop)
 
@@ -47,20 +45,20 @@ class Config:
             _config._reload_task = _config._loop.create_task(_config._schedule_reload())
         return _config
 
-    def get(self, key: str) -> Union[str, dict]:
+    def get(self, key: str) -> Union[str, Dict[str, Any]]:
         return self._get(key)
 
-    def __getitem__(self, key: str) -> Union[str, dict]:
+    def __getitem__(self, key: str) -> Union[str, Dict[str, Any]]:
         return self._get(key)
 
-    def _get(self, key: str) -> Union[str, dict]:
+    def _get(self, key: str) -> Union[str, Dict[str, Any]]:
         """Get value by key and convert it to corresponding type"""
         binary_value = self.cdb.get(key.encode())
         if binary_value is None:
             raise KeyError(f"Key `{key}` is not found in config")
         return self._cast_value(binary_value)
 
-    def __contains__(self, key):
+    def __contains__(self, key: str):
         """Return True if key exists in the config"""
         return key in self.cdb
 
@@ -77,6 +75,9 @@ class Config:
         return self.cdb.values()
 
     async def _schedule_reload(self):
+        if self._reload_interval is None:
+            raise RuntimeError("'reload_interval' must be defined")
+
         while True:
             async with aiofiles.open(self._filename, "rb") as f:
                 _config = await f.read()
@@ -103,20 +104,22 @@ class Config:
                 await self._reload_task
 
     @staticmethod
-    def _cast_value(value: bytes) -> Union[str, dict]:
+    def _cast_value(value: bytes) -> Union[str, Dict[str, Any]]:
         # Decode bytes to unicode
-        value = value.decode()
-        prefix = value[:1]
-        value = value[1:]
+        value_decode = value.decode()
+        prefix = value_decode[:1]
+        value_decode = value_decode[1:]
 
         if prefix == "s":
-            return value
+            return value_decode
         elif prefix == "j":
-            return json.loads(value)
+            return json.loads(value_decode)
         else:
             raise ValueError
 
-    def _flatten_dict(self, d: dict, path: str = "") -> Iterator[Tuple[str, str]]:
+    def _flatten_dict(
+        self, d: Dict[str, Any], path: str = ""
+    ) -> Iterator[Tuple[str, str]]:
         """Return dict items as tuples: (xpath-like key, value)"""
         for key, value in d.items():
             _path = "/".join((path, key))
